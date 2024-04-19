781cf38f7d114dc46d485be1dc36ceca7d12de64
Alessandro <149250167+AleFucar@users.noreply.github.com>
2024-04-19 13:40:31 +0200

    REFACTORING: come indicato dal bonus, eseguito refactoring con il ciclo while al posto dell ciclo for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class CheckGuest {
 
 	//creiamo il metodo main per inizializzare lo script
+	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 		
 		//creiamo l'array di stringhe che equivale ai nomi e cognomi degli invitati
@@ -22,6 +23,7 @@ public class CheckGuest {
 		Guest[8] = "Pardis Zarei";
 		Guest[9] = "Martina Maccherone";
 		Guest[10] = "Rachel Zeilic";
+		
 
 		//inizializziamo in una variabile "scan" l'utility Scanner
 		Scanner scan = new Scanner(System.in);
@@ -33,21 +35,39 @@ public class CheckGuest {
 		String TryGuest =  scan.nextLine();
 		
 		//creiamo un ciclo for dove la "i" è il contatore e Guest.length è la lunghezza dell'array "Guest"
-		for (int i = 0; i < Guest.length; i++) {
-					
-			//Dato il ciclo for, dato ogni indice dell'array "Guest" attraverso ".equals" controlla se il contenuto di ogni indice dell'array è uguale al contenuto della stringa "TryGuest"
+//		for (int i = 0; i < Guest.length; i++) {
+//					
+//			//Dato il ciclo for, dato ogni indice dell'array "Guest" attraverso ".equals" controlla se il contenuto di ogni indice dell'array è uguale al contenuto della stringa "TryGuest"
+//			if (Guest[i].equals(TryGuest)) {
+//				System.out.println("Entra pure!");
+//				break;
+//				
+//			//Se il ciclo arriva all'ultimo indice dell'array e sopra la condizione non si è verificata, allora stampa a video. 
+//			}else if(i == Guest.length-1){
+//				System.out.println("Non sei stato invitato, mi dispiace.");
+//				break;
+//			}
+			
+//		} 
+		
+		
+		
+		int i = 0;
+		
+		while (i < Guest.length) {
+						
 			if (Guest[i].equals(TryGuest)) {
 				System.out.println("Entra pure!");
 				break;
 				
-			//Se il ciclo arriva all'ultimo indice dell'array e sopra la condizione non si è verificata, allora stampa a video. 
 			}else if(i == Guest.length-1){
 				System.out.println("Non sei stato invitato, mi dispiace.");
 				break;
 			}
 			
+			i++;
 		}
-
+		
 	}
 
 }
