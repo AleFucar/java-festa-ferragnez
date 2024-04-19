aece36de921c30957c68da71c55d898332519171
Alessandro <149250167+AleFucar@users.noreply.github.com>
2024-04-19 12:17:17 +0200

    Refactoring del ciclo for e aggiunti commenti

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,10 +1,15 @@
 package com.ferragnez.party;
 
+//Importiamo la utility di java "Scanner" che serve a prendere dei parametri o file in input
 import java.util.Scanner;
 
+//Creiamo una classe parlante
 public class CheckGuest {
 
+	//creiamo il metodo main per inizializzare lo script
 	public static void main(String[] args) {
+		
+		//creiamo l'array di stringhe che equivale ai nomi e cognomi degli invitati
 		String Guest [] = new String [11];
 		Guest[0] = "Dua Lipa";
 		Guest[1] = "Paris Hilton";
@@ -18,21 +23,24 @@ public class CheckGuest {
 		Guest[9] = "Martina Maccherone";
 		Guest[10] = "Rachel Zeilic";
 
-		
+		//inizializziamo in una variabile "scan" l'utility Scanner
 		Scanner scan = new Scanner(System.in);
-				System.out.println("Ciao, prima di entrare, immetti il tuo nome e cognome qui");
+		
+			//stampiamo a video la domanda all'ingresso di ogni persona
+			System.out.println("Ciao, prima di entrare, immetti il tuo nome e cognome qui!");
+			
+		//creiamo una variabile "TryGuest" dove salviamo la stringa in input che immette l'utente attraverso "scan.nextln()"		
 		String TryGuest =  scan.nextLine();
 		
-		//System.out.println(TryGuest);
-		
+		//creiamo un ciclo for dove la "i" è il contatore e Guest.length è la lunghezza dell'array "Guest"
 		for (int i = 0; i < Guest.length; i++) {
-			String string = Guest[i];
-			//System.out.println(Guest[i]);
-			
-			
+					
+			//Dato il ciclo for, dato ogni indice dell'array "Guest" attraverso ".equals" controlla se il contenuto di ogni indice dell'array è uguale al contenuto della stringa "TryGuest"
 			if (Guest[i].equals(TryGuest)) {
 				System.out.println("Entra pure!");
 				break;
+				
+			//Se il ciclo arriva all'ultimo indice dell'array e sopra la condizione non si è verificata, allora stampa a video. 
 			}else if(i == Guest.length-1){
 				System.out.println("Non sei stato invitato, mi dispiace.");
 				break;
